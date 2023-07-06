9353a70de48169c60140bbbe2c1401acbd9ffd4d
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-07-06 22:34:00 +0400

    cors fix try

--- server/index.js
+++ server/index.js
@@ -9,18 +9,16 @@ mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 app.use(express.json());
+app.use(cors());
 
 const todosRouter = require('./routes/todos');
 app.use('/todos', todosRouter);
-app.use(cors());
 
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
-app.use(cors());
 
 const loginRouter = require('./routes/login');
 app.use('/login', loginRouter);
-app.use(cors());
 
 const statisticsRouter = require('./routes/statistics');
 app.use('/statistics', statisticsRouter);
