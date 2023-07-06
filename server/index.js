dc1d6d711078d2a0042b3773a977ff162d005042
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-07-06 22:48:36 +0400

    new fix

--- server/index.js
+++ server/index.js
@@ -10,14 +10,14 @@ const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 app.use(express.json());
 app.use(cors({
-    origin: "https://todo-website-backend.vercel.app"
+    origin: "https://todo-website-zeta.vercel.app"
 }));
 
 const todosRouter = require('./routes/todos');
 app.use('/todos', todosRouter);
 
 app.use(cors({
-    origin: "https://todo-website-backend.vercel.app"
+    origin: "https://todo-website-zeta.vercel.app"
 }));
 
 
@@ -25,7 +25,7 @@ const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
 app.use(cors({
-    origin: "https://todo-website-backend.vercel.app"
+    origin: "https://todo-website-zeta.vercel.app"
 }));
 
 
@@ -36,7 +36,7 @@ const statisticsRouter = require('./routes/statistics');
 app.use('/statistics', statisticsRouter);
 
 app.use(cors({
-    origin: "https://todo-website-backend.vercel.app"
+    origin: "https://todo-website-zeta.vercel.app"
 }));
 
 app.listen(PORT, () => console.log(`Server is up on http://127.0.0.1:${PORT}`));
