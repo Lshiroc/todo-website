38b42c307387703080b97a592a96418164aa5cd0
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-07-08 16:48:54 +0400

    cors issue fixed

--- server/index.js
+++ server/index.js
@@ -9,23 +9,17 @@ mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 app.use(express.json());
-app.use(cors({
-    origin: "https://todo-website-zeta.vercel.app",
-}));
+app.use(cors());
 
 const todosRouter = require('./routes/todos');
 app.use('/todos', todosRouter);
 
-app.use(cors({
-    origin: "https://todo-website-zeta.vercel.app",
-}));
+app.use(cors());
 
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
-app.use(cors({
-    origin: "https://todo-website-zeta.vercel.app",
-}));
+app.use(cors());
 
 const loginRouter = require('./routes/login');
 app.use('/login', loginRouter);
@@ -33,8 +27,6 @@ app.use('/login', loginRouter);
 const statisticsRouter = require('./routes/statistics');
 app.use('/statistics', statisticsRouter);
 
-app.use(cors({
-    origin: "https://todo-website-zeta.vercel.app",
-}));
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Server is up on http://127.0.0.1:${PORT}`));
