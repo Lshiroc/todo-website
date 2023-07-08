4d6d138ffed53f225b065729d758de44340749c9
Lshiroc <zeynalmerdanli4321@gmail.com>
2023-07-08 13:32:31 +0400

    fixed title

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import ReactDOM from 'react-dom/client'
 import App from './../App.jsx'
 import './assets/styles/global.css';
@@ -7,6 +8,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Router>
+      <Helmet>
+        <title>Doin' It</title>
+      </Helmet>
       <App />
     </Router>
   //</React.StrictMode>,
